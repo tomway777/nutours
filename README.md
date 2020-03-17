c53fd1e1159dd080157500420aa29ff0b23909f9
gamal.akbar <gamal.akbar@vascomm.co.id>
2020-03-17 21:08:19 +0700

    add bunch fix middleware

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nutours
+
+#### GIT remote :
--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // midleware
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello from the middleware .😊😊😊');
